84546220a8e58540d9e5fc9c77c931b86d32b639
Mubarak <Ali25mx@dukes.jmu.edu>
2026-09-08 19:46:55 -0400

    Add Language model and get_absolute_url methods (MDN Part 3)

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import uuid
+from django.urls import reverse
 
 # Create your models here.
 class Genre(models.Model):
@@ -10,12 +11,23 @@ class Genre(models.Model):
     )
     def __str__(self):
         return self.name
+class Language(models.Model):
+    name = models.CharField(
+        max_length=200,
+        help_text="Enter the book's natural language (e.g. English, French, Japanese etc.)",
+    )
+
+    def __str__(self):
+        return self.name
 
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, blank=True)
     date_of_death = models.DateField('Died', null=True, blank=True)
+    
+    def get_absolute_url(self):
+        return reverse('author-detail', args=[str(self.id)])
     
     
     def __str__(self):
@@ -26,12 +38,15 @@ class Book(models.Model):
     summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
     isbn = models.CharField('ISBN', max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
     genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.title
     def display_genre(self):
         return ', '.join(genre.name for genre in self.genre.all()[:3])
     display_genre.short_description = 'Genre'
+    def get_absolute_url(self):
+        return reverse('book-detail', args=[str(self.id)])
      
 class BookInstance(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this particular book across whole library")
