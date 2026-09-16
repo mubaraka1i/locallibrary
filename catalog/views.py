599bf6415c251c908aca75bbe83d9dd8746163bd
Mubarak <Ali25mx@dukes.jmu.edu>
2026-09-16 19:28:15 -0400

    Add home page with dynamic content (MDN Part 5)
    Wires up the index URL, view, and templates to display record counts
    for books, copies, and authors. Also completes the challenge tasks:
    overriding the title block, adding a case-insensitive keyword count
    for genres/books, and adding images (one hotlinked, one served via
    the static template tag).

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Book, Author, BookInstance, Genre
+
+def index(request):
+    
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    
+    num_authors = Author.objects.count()
+
+    num_genres_politic = Genre.objects.filter(name__icontains='politic').count()
+    num_books_politic = Book.objects.filter(title__icontains='politic').count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genres_politic': num_genres_politic,
+        'num_books_politic': num_books_politic,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
